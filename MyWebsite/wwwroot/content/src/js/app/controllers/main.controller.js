379227d2c9286f9df85676ac4ed5be5a518d3ccd
Kinga <kinga123kon@gmail.com>
2020-06-05 20:25:34 +0200

    latest improvments

--- MyWebsite/wwwroot/content/src/js/app/controllers/main.controller.js
+++ MyWebsite/wwwroot/content/src/js/app/controllers/main.controller.js
@@ -1,11 +1,44 @@
 ﻿app.controller('mainCtrl', function ($scope) {
 
+    $(document).ready(function () {
+        $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+
+            if (this.hash !== "") {
+
+                event.preventDefault();
+
+                var hash = this.hash;
+
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 900, function () {
+
+                    window.location.hash = hash;
+                });
+            }
+        });
+    })
+
     AOS.init({
         duration: 1200,
     })
 
-    new BeforeAfter({
-        id: '#mySlider'
-    });
+    var slider = new BeerSlider(document.getElementById("slider1"));   
+
+    $scope.onJumbotronBackgroundMouseEnter = function (direction) {
+        slider.reveal(direction);
+    }
+
+
+   $scope.hiddenDivFirst = false;
+   $scope.showDivFirst = function () {
+       $scope.hiddenDivFirst = !$scope.hiddenDivFirst;
+   };
+   
+   $scope.hiddenDivSecond = false;
+   $scope.showDivSecond = function () {
+       $scope.hiddenDivSecond = !$scope.hiddenDivSecond;
+   };
+
 
 });
